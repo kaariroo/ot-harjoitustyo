695a0a15a325fb41342793f0fffbbb85b0f0118a
Roope Kääriäinen <kaariainenroope@gmail.com>
2023-04-03 14:24:50 +0300

    kommentteja

--- hex-grid-app/src/main.py
+++ hex-grid-app/src/main.py
@@ -14,29 +14,30 @@ clock = pygame.time.Clock()
 
 
 
-
+# piirtää kartan alle ja laittaa hexat paikoilleen
 def redraw_game_window():
     win.blit(bg, (0, 0))
     for hex in hex_list:
         hex.draw()
     pygame.display.update()
 
-#mainloop
+#laittaa hexat listaan
 hex_list = []
 for _ in range(600):
-    hex_list.append(Hexagon(win, (255, 255, 255), 6, 200, 0, 0, 1))
+	z = 31
+	hex_list.append(Hexagon(win, (255, 255, 255), 6, z, z, z, 1))
 
-
+#muuttaa hexojen paikat kohdilleen, hexat vähän vituillaan
 hex_column = 0
 hex_row = 0
 for i in hex_list:
     if hex_column % 2 == 0:
-        i.x = 1.5 * hex_column * i.radius
-        i.y = hex_row * (1.75 * i.radius)
+        i.x += 1.5 * hex_column * i.radius
+        i.y += hex_row * (1.75 * i.radius)
                 
     if hex_column % 2 != 0:
-        i.x = 1.5 * hex_column * i.radius
-        i.y = hex_row * (1.75 * i.radius) + (0.9 * i.radius)
+        i.x += 1.5 * hex_column * i.radius
+        i.y += hex_row * (1.75 * i.radius) + (0.9 * i.radius)
     hex_row += 1
     if hex_row >= 20:
         hex_column += 1
