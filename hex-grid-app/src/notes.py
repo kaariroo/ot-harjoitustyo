a95efa13e1516761847eb5d36f84c8dc6fa948ab
Roope Kääriäinen <kaariainenroope@gmail.com>
2023-04-20 16:48:28 +0300

    tekstitiedostot tallennetaan omaan kansioon

--- hex-grid-app/src/notes.py
+++ hex-grid-app/src/notes.py
@@ -4,8 +4,12 @@ class Note:
     def __init__(self, hexa):
         self.hexa = hexa
     def write(self, hex_number):
-        if not os.path.isfile(f"{hex_number}.txt"):
-            with open(f"{hex_number}.txt", "w") as document:
+        dirname = os.path.dirname(__file__)
+        print(dirname)
+        notes_file_path = os.path.join(dirname, "hex_notes", f"{hex_number}.txt")
+        print(notes_file_path)
+        if not os.path.isfile(notes_file_path):
+            with open(notes_file_path, "w") as document:
                 document.write("Notes about hex.")
-        os.system(f"gedit {hex_number}.txt")
+        os.system(f"gedit {notes_file_path}")
            
