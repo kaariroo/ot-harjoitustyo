8e2a5185c43e5afe7df1f64d15dfece551fcd909
Roope Kääriäinen <kaariainenroope@gmail.com>
2023-04-25 15:47:06 +0300

    testien kirjoittamista

--- hex-grid-app/src/tests/hexmap_test.py
+++ hex-grid-app/src/tests/hexmap_test.py
@@ -2,6 +2,9 @@ import unittest
 import pygame
 from hexmap import Hexmap
 from hexagon import Hexagon
+from clock import Clock
+from main import MainLoop
+from eventqueue import EventQueue
 
 
 class Testhexmap(unittest.TestCase):
@@ -10,3 +13,8 @@ class Testhexmap(unittest.TestCase):
 
     def test_hexlistassa_on_n_oliota(self):
         self.assertEqual(len(self.hexmap.hexlist), 600)
+
+    def test_findhex(self):
+        hexa = self.hexmap.find_hex(15, 15)
+
+        self.assertEqual(hexa, self.hexmap.hexlist[0])
